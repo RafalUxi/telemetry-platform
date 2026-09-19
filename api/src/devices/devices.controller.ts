25165720828ee67b2aaf6e1ef651050e2d2dcfd4
Rafal <277219772+RafalUxi@users.noreply.github.com>
2026-09-19 11:39:20 +0200

    feat(api): create a device with a generated device_id through POST /devices

--- api/src/devices/devices.controller.ts
+++ api/src/devices/devices.controller.ts
@@ -1,11 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard.js';
-import { Get, UseGuards, HttpCode, Req } from '@nestjs/common';
+import {
+  Get,
+  UseGuards,
+  BadRequestException,
+  Post,
+  HttpCode,
+  Req,
+} from '@nestjs/common';
 import { DevicesService } from './devices.service.js';
+import { z } from 'zod';
+
+const devZodSchema = z.object({
+  name: z.string(),
+});
 
 @Controller('devices')
 export class DevicesController {
   constructor(private readonly dev: DevicesService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UseGuards(AuthGuard)
+  async createOneDevice(
+    @Body() body: unknown,
+    @Req() request: { user: { sub: string } },
+  ) {
+    const parse = devZodSchema.safeParse(body);
+    if (!parse.success) {
+      throw new BadRequestException(parse.error.issues);
+    }
+    const output = await this.dev.createOneDevice(
+      parse.data.name,
+      request.user.sub,
+    );
+    return output;
+  }
 
   @Get() // Get -> without prefix
   @HttpCode(200)
--- api/src/devices/devices.service.ts
+++ api/src/devices/devices.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Inject, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Inject, ConflictException } from '@nestjs/common';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from '../db/schema.js';
 import { DB } from '../db/db.module.js';
 import { eq } from 'drizzle-orm';
+import { randomBytes } from 'node:crypto';
 
 @Injectable()
 export class DevicesService {
@@ -12,12 +13,38 @@ export class DevicesService {
     const data = await this.db
       .select({
         device_id: schema.devices.device_id,
+        name: schema.devices.name,
+        id: schema.devices.id,
       })
       .from(schema.devices)
       .where(eq(schema.devices.user_id, user_id));
     const output = data.map((d) => {
-      return d.device_id;
+      return { name: d.name, devices: d.device_id, id: d.id };
     });
     return output;
+  }
+
+  public async createOneDevice(name: string, user_id: string) {
+    const randomPrefix = randomBytes(6).toString('base64url');
+
+    const device_id = 'dev_' + randomPrefix;
+    try {
+      const [data] = await this.db
+        .insert(schema.devices)
+        .values({ user_id: user_id, device_id: device_id, name: name })
+        .returning({
+          id: schema.devices.id,
+          device_id: schema.devices.device_id,
+          name: schema.devices.name,
+        });
+      return { id: data.id, deviceId: data.device_id, name: data.name };
+    } catch (err) {
+      if ((err as { cause?: { code?: string } }).cause?.code === '23505') {
+        throw new ConflictException(
+          'Device Id already exist -> creat device again',
+        );
+      }
+      throw err;
+    }
   }
 }
